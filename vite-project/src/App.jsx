be353e8335049b495ebbc3d36007902bb8bbd55c
Adrian Lawson <adrianglawson@gmail.com>
2023-02-14 17:18:07 -0800

    enable functionality

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,157 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import ethers from 'ethers'
+import {ethers, toBigInt} from 'ethers'
+import {abi, toNumber, colors} from './config' 
+
+const contractAddress = '0x1e6b0A6DF08602C6039f030cC4D89C44495A7bf1'
+let provider, signer, contract
 
 function App() {
-  const [count, setCount] = useState(0)
+  provider = new ethers.BrowserProvider(window.ethereum)
+  contract = new ethers.Contract(contractAddress, abi, provider)
+
+  const [userAddress, setUserAddress] = useState('')
+  const [outputHTML, setOutputHTML] = useState('')
+  const [display, setDisplay] = useState('block')
+
+  async function init(e) {
+    if(e) e.preventDefault()
+
+    if(window.ethereum == null) {
+      alert('Browser wallet not installed')
+      throw 'Browser wallet not installed'
+    }
+
+    await provider.send("eth_requestAccounts", [])
+    signer = await provider.getSigner()
+
+    await enforceNetwork((await provider.getNetwork()).chainId)
+
+    setUserAddress(await signer.getAddress())
+    setDisplay('none')
+    //await getPastResults()
+  }
+
+  function handleEvent(player, guess, result, attempts) {
+    let html = outputHTML
+
+    let res = [];
+    for(let i = 0; i < result.length; i++) {
+      let value = parseInt(result[i])
+      console.log(value)
+      res.push(value)
+
+      html += `<span style="background-color: ${colors[value]}; padding-right: 2.5%; padding-left: 2.5%; border: 1px; font-size: 200%;">${toNumber[guess[i]]}</span>`
+    }
+
+    html += `<br/>`
+    setOutputHTML(html)
+    document.getElementById('outputHTML').innerHTML = outputHTML
+
+  
+    if(instances(res, 1) === 5) {
+      alert(`You won on attempt ${attempts}`)
+    }
+  }
+
+  function instances(arr, value) {
+    let count = 0
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] == value) {
+        count++
+      }
+    }
+
+    return count
+  }
+
+  // Begin listening for event
+  contract.on("guessed", (player, guess, result, attempts, won) => {
+    console.log(player, guess, result, attempts)
+    handleEvent(player, guess, result, attempts)
+  });
+
+  async function getPastResults() {
+    // Query all time for any transfer to ethers.eth
+    // let filter = contract.filters.guessed(userAddress)
+    // let events = await contract.queryFilter(filter)
+    // console.log(events)
+    // Calculate the timestamp for 24 hours ago
+    const now = Math.floor(Date.now() / 1000);
+    const twentyFourHoursAgo = now - 24 * 60 * 60;
+    let tfha = ethers.encodeBytes32String(twentyFourHoursAgo.toString())
+    // Create a filter for events emitted in the last 24 hours
+    const filter = {
+      address: contractAddress,
+      fromBlock: 0,
+      toBlock: "latest",
+      topics: [],
+      blockhash: null,
+    };
+
+    // Add a filter for the timestamp of the event
+    const eventName = "guessed";
+    const eventTopic = ethers.id(eventName);
+    filter.topics.push(eventTopic);
+    filter.topics.push(null);
+    filter.topics.push(ethers.zeroPadBytes(tfha, 32));
+    
+    // Retrieve the events that match the filter
+    const events = await contract.queryFilter("guessed");
+    console.log(events);
+  }
+
+  async function enforceNetwork(current) {
+    if(parseInt(current) != 5) {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{
+            chainId: "0x5"
+        }]
+      });
+    }
+  }
+
+  // function toNumber(word) {
+  //   let nums = []
+
+  //   for(let i = 0; i < 5; i++) {
+  //     nums.push(toNumber[word[i]])
+  //   }
+    
+  //   return nums
+  // }
+
+  async function guess() {
+    await init()
+
+    const word = document.getElementById('word').value
+    let nums = []
+    for(let i = 0; i < word.length; i++) {
+      nums.push(toNumber.indexOf(word[i]))
+    }
+    console.log(nums)
+    const tx = await contract.connect(signer).guess(nums, {value: 0})
+    await tx.wait(5)
+    console.log(tx)
+  }
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <h1 style={{marginTop: '-100%'}}>WORDL3</h1> */}
       </div>
-      <h1>Vite + React</h1>
+      <div dangerouslySetInnerHTML={{__html: outputHTML}}></div>
+      {/* <span style={{backgroundColor: 'red'}}>W</span>
+      <span style={{backgroundColor: 'green'}}>O</span>
+      <span style={{backgroundColor: 'yellow'}}>R</span>
+      <span style={{backgroundColor: 'black'}}>D</span> */}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <input type='text' placeholder='5 Letter Word. . .' id='word'></input>
+      <button onClick={() => guess()}>Submit</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => init()} style={{display: display}}>Connect Wallet</button>
+      
     </div>
   )
 }
---
+++ vite-project/src/config.js
@@ -0,0 +1,300 @@
+export const abi = [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint8[5]",
+        "name": "guess",
+        "type": "uint8[5]"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint8[5]",
+        "name": "result",
+        "type": "uint8[5]"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "attempts",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "won",
+        "type": "bool"
+      }
+    ],
+    "name": "guessed",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "attemptsToday",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "endTime",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint8[5]",
+        "name": "letters",
+        "type": "uint8[5]"
+      }
+    ],
+    "name": "guess",
+    "outputs": [
+      {
+        "internalType": "uint8[5]",
+        "name": "",
+        "type": "uint8[5]"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint8[5]",
+        "name": "baseArray",
+        "type": "uint8[5]"
+      },
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "includes",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "lastAttempt",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint8[5]",
+        "name": "baseArray",
+        "type": "uint8[5]"
+      },
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "occurences",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "record",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint8[5]",
+        "name": "letters",
+        "type": "uint8[5]"
+      }
+    ],
+    "name": "setWord",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "won",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+
+export const toNumber = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ]
+
+export const colors = [
+  'red',
+  'green',
+  'yellow'
+]
